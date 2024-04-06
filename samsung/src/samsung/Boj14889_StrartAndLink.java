510c47187b99883a8a9d962e16eaff6ce2e3d2d7
changuk2 <changuk0308@naver.com>
2024-04-07 00:50:41 +0900

    1,2주차 스터디 복습

--- samsung/src/samsung/Boj14889_StrartAndLink.java
+++ samsung/src/samsung/Boj14889_StrartAndLink.java
@@ -31,28 +31,38 @@ public class Boj14889_StrartAndLink {
 	    }
 
 	    // 조합을 통해 팀 나누기
-	    public static void combination(int start, int depth) {
+	    public static void combination(int index, int depth) {
+	    	//1. 팀을 나눴으면
 	        if (depth == n / 2) {
+	        	//2. 차이 구하기
 	            calculateDifference();
 	            return;
 	        }
 
-	        for (int i = start; i < n; i++) {
+	        for (int i = index; i < n; i++) {
+	        	//방문x-> 방문 처리: 추후 startTeam에서 활용
 	            if (!visited[i]) {
-	                visited[i] = true;
+	                visited[i] = true; //방문 처리
 	                combination(i + 1, depth + 1);
-	                visited[i] = false;
+	                visited[i] = false; //재귀가 끝나면 비방문 처리
 	            }
 	        }
 	    }
 
 	    // 두 팀의 능력치 차이 계산
+	    
+	    /*선수 0, 1, 2가 스타트 팀인 경우:
+	     * 스타트 팀 능력치: (s[0][1] + s[1][0]) + (s[0][2] + s[2][0]) + s[1][2] + s[2][1] = 1 + 1 + 2 + 2 + 2 + 2 = 10
+	     * 링크 팀 능력치: s[3][4] + s[4][3] + s[3][5] + s[5][3] + s[4][5] + s[5][4] = 4 + 4 + 5 + 5 + 5 + 5 = 28
+	     * 차이: |10 - 28| = 18
+	     */
 	    public static void calculateDifference() {
 	        int startTeam = 0;
 	        int linkTeam = 0;
 
 	        for (int i = 0; i < n - 1; i++) {
 	            for (int j = i + 1; j < n; j++) {
+	            	//i번째 사람과 j번째 사람이 true면 startTra
 	                if (visited[i] && visited[j]) {
 	                    startTeam += s[i][j] + s[j][i];
 	                } else if (!visited[i] && !visited[j]) {
--- samsung/src/samsung/Review.java
+++ samsung/src/samsung/Review.java
@@ -3,103 +3,148 @@ package samsung;
 import java.util.*;
 import java.io.*;
 
-//복습용 클래스
-//1번 시험감독
-
 
 public class Review {
-	//1번 시험감독
-//	static int n; //시험장 수
-//	static int[] a;
-//	static int b,c; //학생 수(a),각 총감독 할당 수(b),각 보조감독 할당수(c)
 	
-	//2번 퇴사
-	static int n;   //n일
-	static int[] t; //기간
-	static int[] p; //금액
-	static int[] dp; //동적 프로그래밍
-	static int maxResult=Integer.MIN_VALUE; //최대 금액
-	public static void main(String args[]) throws IOException
+	
+	static int[][] gear=new int [4][8];
+	
+	static String[] stringGear=new String[4];
+	
+	static int k;// 회전 횟수
+	static int[] selectedGear; //회전 시킬 톱니바퀴
+	static int[] way; //시계, 반시계 방법
+	
+	//시계 회전
+	static void turnClock(int[] gear)
 	{
+		int temp=gear[7];
+		for(int i=7;i>0;i--)
+		{
+			gear[i]=gear[i-1];
+		}
+		gear[0]=temp;
+	}
+	
+	//반 시계 회전
+	static void turnAntiClock(int[] gear)
+	{
+		int temp=gear[0];
+		for(int i=0;i<7;i++)
+		{
+			gear[i]=gear[i+1];
+		}
+		gear[7]=temp;
+	}
+	
+	public static void main(String args[]) throws IOException{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		//1번 시험감독
-//		
-//		//학생수 입력
-//		n=Integer.parseInt(br.readLine());
-//		
-//		//시험장수 입력
-//		a=new int[n];
-//		st=new StringTokenizer(br.readLine());
-//		for(int i=0;i<n;i++)
-//		{
-//			a[i]=Integer.parseInt(st.nextToken());
-//		}
-//		//b,c 입력
-//		st=new StringTokenizer(br.readLine());
-//		b=Integer.parseInt(st.nextToken());
-//		c=Integer.parseInt(st.nextToken());
-//		
-//		//비즈니스 로직 구현
-//		long cnt=n;//보조 감독 수 
-//		
-//		for(int i=0;i<n;i++)
-//		{
-//			//총 감독관 보다 작거나 같으면 보조감독은 필요 없다
-//            if(a[i]<b)
-//            {
-//                continue;
-//            }
-//			//나머지가 있는 경우
-//			if(((a[i]-b)%c)>0)
-//			{
-//				cnt+=(a[i]-b)/c+1;
-//			}
-//			//나머지가 없는 경우
-//			else
-//			{
-//				cnt+=(a[i]-b)/c;
-//			}
-//		}
-//		
-//		System.out.println(cnt); //결과
 		
-//		//2번 퇴사
-//		
-//		//n일 입력
-//		n=Integer.parseInt(br.readLine());
-//		
-//		//기간,금액 입력
-//		t=new int[n];
-//		p=new int[n];
-//		for(int i=0;i<n;i++)
-//		{
-//			st=new StringTokenizer(br.readLine());
-//			t[i]=Integer.parseInt(st.nextToken());
-//			p[i]=Integer.parseInt(st.nextToken());
-//		}
-//		
-//		//비즈니스 로직 구현
-//		dp=new int[n+1];
-//		for(int i=0;i<n;i++)
-//		{
-//			//상담을 선택한 경우
-//			//i일에 상담을 선택했을때 최대로 얻을 수 있는 수익
-//			if(i+t[i]<n+1)
-//			{
-//				dp[i+t[i]]=Math.max(dp[i+t[i]],dp[i]+p[i]);
-//			}
-//			//상담을 선택하지 않은 경우
-//			//다음날은 그 전날과 비교 했을시 더큰지 확인
-//			dp[i+1]=Math.max(dp[i+1], dp[i]);
-//			
-//		}
-//	
-//		System.out.println(dp[n]); //n일차
-		
-		//3번 1이 될때까지
+		//톱니 바퀴 상태 입력
+		for(int i=0;i<4;i++)
+		{
+			stringGear[i]=br.readLine(); //스트링 형태로 받은 후	
+		}
+		//톱니 바퀴 
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<8;j++)
+			{
+				gear[i][j]=stringGear[i].charAt(j)-'0';
+			}
+		}
 		
 		
+		//k 입력
+		k=Integer.parseInt(br.readLine());
+		//회전시킬 톱니바퀴, 회전 방법 입력
+		selectedGear=new int[k];
+		way=new int[k];
+		for(int i=0;i<k;i++)
+		{
+			st=new StringTokenizer(br.readLine());
+			selectedGear[i]=Integer.parseInt(st.nextToken())-1;
+			way[i]=Integer.parseInt(st.nextToken());
+		}
+	
+		//비즈니스 로직 구현
+		
+		//톱니 바퀴 회전
+		for(int i=0;i<k;i++)
+		{
+			boolean[] rotated=new boolean[4]; //각 톱니바퀴 회전여부
+			int[] dirs=new int[4];//각 톱니바퀴의 회전 방향 저장
+			
+			dirs[selectedGear[i]]=way[i];  //선택된 톱니바퀴의 초기 회전 방향 저장
+			
+			
+			//선택된 톱니바퀴의 오른쪽 톱니바퀴 쭉 상태 확인
+			for(int j=selectedGear[i]+1;j<4;j++)
+			{
+				//만약 선택된 톱니바퀴의 오른쪽이 다르면
+				if(gear[j-1][2]!=gear[j][6]) //나,오른쪽 비교
+				{
+					dirs[j]=-dirs[j-1];//오른쪽 톱니 바퀴 방향 변경
+				}
+				//같으면 회전을 안하므로 상태 확인종료
+				else {
+					break;
+				}
+			}
+			
+			
+			//선택된 톱니바퀴의 왼쪽 톱니바퀴들 상태 확인
+			for(int j=selectedGear[i]-1;j>=0;j--)
+			{
+				//만약 선택된 톱니바퀴의 왼쪽이 다르면
+				if(gear[j+1][6]!=gear[j][2]) //나,왼쪽 비교
+				{
+					dirs[j]=-dirs[j+1];//왼쪽 톱니 바퀴 방향 변경
+				}
+				//같으면 회전을 안하므로 상태 확인종료
+				else {
+					break;
+				}
+			}
+
+			
+			//실제 회전
+			for(int j=0;j<4;j++)
+			{
+				//시계 방향 회전
+				if(dirs[j]==1)
+				{
+					turnClock(gear[j]);
+				}
+				else if(dirs[j]==-1)
+				{
+					turnAntiClock(gear[j]);
+				}				
+			}
+		
+		}
+	
+		//결과
+		int result=0;
+		if(gear[0][0]==1)
+		{
+			result+=1;
+		}
+		if(gear[1][0]==1)
+		{
+			result+=2;
+		}
+		if(gear[2][0]==1)
+		{
+			result+=4;
+		}
+		if(gear[3][0]==1)
+		{
+			result+=8;
+		}
+	
+		System.out.println(result);
 		
 	}
 }
